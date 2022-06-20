d9af33ae3fecbe79397589983b97eca313671817
Samik Roy <samik.n.roy@gmail.com>
2022-06-20 18:07:04 +0530

    Update syncTORIps.ps1

--- syncTORIps.ps1
+++ syncTORIps.ps1
@@ -23,7 +23,7 @@ git pull  2>&1 | write-host
 git status 2>&1 | write-host
 
 "Update the file $file"
-Add-Content -Path $file -Value $jsonContent 
+Add-Content -Path $file -Value $csvFileData
 
 "Status prior to stage"
 git status 2>&1 | write-host
